8d53f991f92d5080eaac5e3dc4372bc04685820a
DraskyChen <82917000+DraskyChen@users.noreply.github.com>
2026-08-16 09:40:05 +0000

    Deploying to gh-pages from @ DraskyChen/MyBlog@2db523553fa7335f72f626736ede54c4635e90a6 🚀

---
+++ assets/ai_DeepLearning_线性神经网络_index.md.B-D_6RLX.js
@@ -0,0 +1,8 @@
+import{_ as s,o as e,c as n,a3 as i}from"./chunks/framework.D81Tvp8W.js";const _=JSON.parse('{"title":"线性神经网络","description":"","frontmatter":{"title":"线性神经网络","date":"2025-09-02T00:00:00.000Z","categories":["ai","DeepLearning","线性神经网络"],"tags":["AI","深度学习","d2l","DeepLearning"]},"headers":[],"relativePath":"ai/DeepLearning/线性神经网络/index.md","filePath":"ai/DeepLearning/线性神经网络/index.md","lastUpdated":1786872962000}'),t={name:"ai/DeepLearning/线性神经网络/index.md"};function p(r,a,o,c,l,d){return e(),n("div",null,[...a[0]||(a[0]=[i(`<h1 id="线性神经网络" tabindex="-1">线性神经网络 <a class="header-anchor" href="#线性神经网络" aria-label="Permalink to &quot;线性神经网络&quot;">​</a></h1><p></p><p>在介绍深度神经网络之前，我们需要了解神经网络训练的基础知识。 本章我们将介绍神经网络的整个训练过程， 包括：定义简单的神经网络架构、数据处理、指定损失函数和如何训练模型。 为了更容易学习，我们将从经典算法————<em>线性</em>神经网络开始，介绍神经网络的基础知识。 经典统计学习技术中的线性回归和softmax回归可以视为线性神经网络， 这些知识将为本书其他部分中更复杂的技术奠定基础。</p><div class="language-toc vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">toc</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span></span></span>
+<span class="line"><span>linear-regression</span></span>
+<span class="line"><span>linear-regression-scratch</span></span>
+<span class="line"><span>linear-regression-concise</span></span>
+<span class="line"><span>softmax-regression</span></span>
+<span class="line"><span>image-classification-dataset</span></span>
+<span class="line"><span>softmax-regression-scratch</span></span>
+<span class="line"><span>softmax-regression-concise</span></span></code></pre></div>`,4)])])}const m=s(t,[["render",p]]);export{_ as __pageData,m as default};
---
+++ assets/ai_DeepLearning_计算性能_index.md.Bcms6viz.js
@@ -0,0 +1,8 @@
+import{_ as n,o as e,c as s,a3 as p}from"./chunks/framework.D81Tvp8W.js";const u=JSON.parse('{"title":"计算性能","description":"","frontmatter":{"title":"计算性能","date":"2025-09-02T00:00:00.000Z","categories":["ai","DeepLearning","计算性能"],"tags":["AI","深度学习","d2l","DeepLearning"]},"headers":[],"relativePath":"ai/DeepLearning/计算性能/index.md","filePath":"ai/DeepLearning/计算性能/index.md","lastUpdated":1786872962000}'),t={name:"ai/DeepLearning/计算性能/index.md"};function i(l,a,r,c,o,d){return e(),s("div",null,[...a[0]||(a[0]=[p(`<h1 id="计算性能" tabindex="-1">计算性能 <a class="header-anchor" href="#计算性能" aria-label="Permalink to &quot;计算性能&quot;">​</a></h1><p></p><p>在深度学习中，数据集和模型通常都很大，导致计算量也会很大。 因此，计算的性能非常重要。 本章将集中讨论影响计算性能的主要因素：命令式编程、符号编程、 异步计算、自动并行和多GPU计算。 通过学习本章，对于前几章中实现的那些模型，可以进一步提高它们的计算性能。 例如，我们可以在不影响准确性的前提下，大大减少训练时间。</p><div class="language-toc vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">toc</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span></span></span>
+<span class="line"><span>hybridize</span></span>
+<span class="line"><span>async-computation</span></span>
+<span class="line"><span>auto-parallelism</span></span>
+<span class="line"><span>hardware</span></span>
+<span class="line"><span>multiple-gpus</span></span>
+<span class="line"><span>multiple-gpus-concise</span></span>
+<span class="line"><span>parameterserver</span></span></code></pre></div>`,4)])])}const m=n(t,[["render",i]]);export{u as __pageData,m as default};
